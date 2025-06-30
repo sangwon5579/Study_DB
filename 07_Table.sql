0522ff6f1102b63e3e113bef1e440496a3174fe0
sangwon5579 <chosw0410@naver.com>
2025-06-30 15:04:28 +0900

    table 생성 방법

--- 07_Table.sql
+++ 07_Table.sql
@@ -13,7 +13,17 @@ INSERT INTO tb_member VALUES (1, 'user1', '1234', '홍길동', '2025-06-30');
 INSERT INTO tb_member VALUES (2, 'user2', '1234', '이몽룡', CURDATE());
 INSERT INTO tb_member VALUES (3, 'user3', '1234', '성춘향', DEFAULT);
 INSERT INTO tb_member(mem_no, mem_id) VALUES (4, 'user4');
+INSERT INTO tb_member VALUES (NULL, NULL, NULL, NULL, NULL);
 
 SELECT *
 FROM tb_member;
 
+DROP TABLE `tb_member`; #테이블 삭제
+
+CREATE TABLE `tb_member`(
+	`mem_no` INT NOT NULL,
+	`mem_id` VARCHAR(20) NOT NULL,
+	`mem_pass` VARCHAR(20) NOT NULL,
+	`mem_name` varchar(10) NOT NULL,
+	`enroll_date` DATE DEFAULT CURDATE()
+);
